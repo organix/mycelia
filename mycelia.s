b68bb624c9187624aaf9d33aae7eeba3eea2fb7e
Dale Schumacher <dalnefre@yahoo.com>
2014-07-20 22:15:41 -0500

    Basic memory management
    untested

--- mycelia.s
+++ mycelia.s
@@ -1,3 +1,6 @@
+@
+@ mycelia.s -- A bare-metal actor kernel for Raspberry Pi
+@
 @ Copyright 2014 Dale Schumacher, Tristan Slominski
 @
 @ Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,10 +21,81 @@
 @ If your tabs are set correctly, the lines above should be aligned.
 @
 
-@ mycelia is the entry point for the kernel
+@ Special register usage:
+@   fp (r11) the event being processed, including the message and target actor
+@   sl (r10) the sponsor providing resources for this computation
+
+@ mycelia is the entry point for the actor kernel
 	.text
-	.align 2		@ alignment 2^n (2^2 = 4 byte alignment)
+	.align 2		@ alignment 2^n (2^2 = 4 byte machine word)
 	.global mycelia
 mycelia:
-	bl monitor		@ monitor();
-	b mycelia
+	bl	monitor		@ monitor();
+	b	mycelia
+
+reserve:		@ reserve a block (32 bytes) of memory
+	ldr	r1, =block_free	@ address of free list pointer
+	ldrs	r0, [r1]	@ address of first free block
+	beq	1f		@ if not null
+	ldr	r2, [r0]	@	follow link to next free block
+	str	r2, [r1]	@	update free list pointer
+	bx	lr		@	return
+1:				@ else
+	ldr	r1, =block_end	@	address of block end pointer
+	ldr	r0, [r1]	@	address of new memory block
+	bl	release		@	"free" new memory block
+	b	reserve		@	try again
+
+release:		@ release the memory block pointed to by r0
+	stmdb	sp!, {r4-r9,lr}	@ preserve in-use registers
+	ldr	r1, =block_free	@ address of free list pointer
+	ldr	r2, [r1]	@ address of next free block
+	str	r0, [r1]	@ update free list pointer
+	ldr	r1, =block_clr	@ address of block-erase pattern
+	ldmia	r1, {r3-r9}	@ read 7 words (32 - 4 bytes)
+	stmia	r0, {r2-r9}	@ write 8 words (incl. next free block pointer)
+	ldmia	sp!, {r4-r9,pc}	@ restore in-use registers and return
+
+enqueue:		@ enqueue event pointed to by r0
+	ldr	r1, sl, #1024	@ event queue head/tail indicies
+	uxtb	r2, r1, #8	@ get head index
+	uxtb	r3, r1, #16	@ get tail index
+	str	r0, [sl,r3,LSL #2] @ store event pointer at tail
+	add	r3, r3, #1	@ advance tail
+	cmp	r2, r3		@ if queue full
+	beq	halt		@	KERNEL PANIC!
+	strb	r3, sl, #1026	@ update tail index
+	bx	lr		@ return
+
+dequeue:		@ dequeue next event from queue
+	ldr	r1, sl, #1024	@ event queue head/tail indicies
+	uxtb	r2, r1, #8	@ get head index
+	uxtb	r3, r1, #16	@ get tail index
+	cmp	r2, r3		@ if queue empty
+	moveq	r0, #0		@	return null
+	ldrne	r0, [sl,r2,LSL #2] @ else
+	addne	r2, r2, #1	@	advance head
+	strneb	r2, sl, #1025	@	update head index
+	bx	lr		@	return event pointer
+
+	.data
+	.align 5		@ align to cache-line
+sponsor_0:
+	.space 256*4		@ event queue (offset 0)
+	.int 0			@ queue head/tail (offset 1024)
+
+	.rodata
+	.align 5		@ align to cache-line
+block_clr:
+	.ascii "Who is licking my HONEYPOT?\0"
+
+	.data
+	.align 2		@ align to machine word
+block_free:
+	.int 0			@ pointer to next free block, 0 if none
+block_end:
+	.int heap_start		@ pointer to end of block memory
+
+	.bss
+	.align 5		@ align to cache-line
+heap_start:
