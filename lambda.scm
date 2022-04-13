78330aed1fa9416635abe40c42dcfdf51b173996
Dale Schumacher <dalnefre@yahoo.com>
2022-04-13 16:49:39 -0600

    disable evaluation tracing

--- lambda.scm
+++ lambda.scm
@@ -7,12 +7,12 @@
 (define empty-env
   (CREATE
     (BEH (cust _index)
-      (seq (print 'empty-env) (debug-print _index))
+      ;(seq (print 'empty-env) (debug-print _index))
       (SEND cust #undefined))))
 (define bound-beh
   (lambda (value next)
     (BEH (cust index)
-      (seq (print 'bound-beh) (debug-print index))
+      ;(seq (print 'bound-beh) (debug-print index))
       (define index (- index 1))
       (if (zero? index)
         (SEND cust value)
@@ -25,19 +25,19 @@
 (define const-beh
   (lambda (value)
     (BEH (cust _)             ; eval
-      (seq (print 'const-beh) (debug-print value))
+      ;(seq (print 'const-beh) (debug-print value))
       (SEND cust value))))
 (define var-beh
   (lambda (index)
     (BEH (cust env)           ; eval
-      (seq (print 'var-beh) (debug-print index))
+      ;(seq (print 'var-beh) (debug-print index))
       (SEND env (list cust index)))))
 
 ; apply: (cust param env)
 (define op-lambda             ; (lambda <body>)
   (CREATE
     (BEH (cust body . opt-env)
-      (seq (print 'op-lambda) (debug-print (list* cust body opt-env)))
+      ;(seq (print 'op-lambda) (debug-print (list* cust body opt-env)))
       (if (null? opt-env)
         (SEND cust SELF)      ; eval
         (SEND cust            ; apply
@@ -46,7 +46,7 @@
 (define oper-beh
   (lambda (body)
     (BEH (cust arg . opt-env)
-      (seq (print 'oper-beh) (debug-print (list* cust arg opt-env)))
+      ;(seq (print 'oper-beh) (debug-print (list* cust arg opt-env)))
       (if (null? opt-env)
         (SEND cust SELF)      ; eval
         (SEND body            ; apply
@@ -55,7 +55,7 @@
 (define appl-beh
   (lambda (oper senv)
     (BEH (cust param . opt-env)
-      (seq (print 'appl-beh) (debug-print (list* cust param opt-env)))
+      ;(seq (print 'appl-beh) (debug-print (list* cust param opt-env)))
       (if (null? opt-env)
         (SEND cust SELF)      ; eval
         (SEND param           ; apply
@@ -64,20 +64,20 @@
 (define k-apply-beh
   (lambda (cust oper env)
     (BEH arg
-      (seq (print 'k-apply-beh) (debug-print arg))
+      ;(seq (print 'k-apply-beh) (debug-print arg))
       (SEND oper
         (list cust arg env)))))
 
 (define comb-beh
   (lambda (comb param)
     (BEH (cust env)           ; eval
-      (seq (print 'comb-beh) (debug-print (list comb param)))
+      ;(seq (print 'comb-beh) (debug-print (list comb param)))
       (SEND comb
         (list (CREATE (k-call-beh cust param env)) env)))))
 (define k-call-beh
   (lambda (cust param denv)
     (BEH oper
-      (seq (print 'k-call-beh) (debug-print oper))
+      ;(seq (print 'k-call-beh) (debug-print oper))
       (SEND oper
         (list cust param denv)))))
 
@@ -87,14 +87,14 @@
 (define a-test-eval
   (CREATE
     (BEH _
-      (seq (print 'empty-env) (debug-print empty-env))
-      (seq (print 'op-lambda) (debug-print op-lambda))
+      ;(seq (print 'empty-env) (debug-print empty-env))
+      ;(seq (print 'op-lambda) (debug-print op-lambda))
       (define var-1 (CREATE (var-beh 1)))
-      (seq (print 'var-1) (debug-print var-1))
+      ;(seq (print 'var-1) (debug-print var-1))
       (define const-42 (CREATE (const-beh 42)))
-      (seq (print 'const-42) (debug-print const-42))
+      ;(seq (print 'const-42) (debug-print const-42))
       (define fn-id (CREATE (comb-beh op-lambda var-1)))
-      (seq (print 'fn-id) (debug-print fn-id))
+      ;(seq (print 'fn-id) (debug-print fn-id))
       (define expr
         (CREATE (comb-beh
           fn-id
